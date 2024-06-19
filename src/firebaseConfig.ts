7bfad35777c5a04d72397621ae8ee517cc45f2e5
Haresh <lakhwaniharesh134@gmail.com>
2024-06-20 00:37:46 +0530

    Changes in auth background and text input design

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC7bNfYHXLYnMK8oMKlsCUrq0nrCpcfzWA",
-    authDomain: "sanatan-stories-c19c9.firebaseapp.com",
-    projectId: "sanatan-stories-c19c9",
-    storageBucket: "sanatan-stories-c19c9.appspot.com",
-    messagingSenderId: "677566200219",
-    appId: "1:677566200219:web:291928affd457b17ec093e",
-    measurementId: "G-YX560DHSGB"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
