55cd815f79284596cea835001ebb0f5fbe3490ee
Haresh <lakhwaniharesh134@gmail.com>
2024-07-06 16:26:40 +0530

    Implemented chanting and analizing text feature

--- functions/src/temples/generateStory.ts
+++ functions/src/temples/generateStory.ts
@@ -2,7 +2,7 @@ import * as functions from 'firebase-functions';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 
-const genAI = new GoogleGenerativeAI("AIzaSyBpZs6pcBEwfm3iNVWlqKtfpYqpIYxU26Q");
+const genAI = new GoogleGenerativeAI("");
 
 export const generateStory = functions.https.onCall(async (data, context) => {
   const { templeName } = data;
